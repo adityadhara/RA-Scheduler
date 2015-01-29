a4c105b1673bbbf6a880bd5ad9561e90a2d8303a
george <gbrova@gmail.com>
2015-01-29 12:58:03 -0600

    added placeholders for viewing (user role) and managing (administrator role)

--- main/views.py
+++ main/views.py
@@ -5,4 +5,13 @@ from main import app
 @app.route('/index')
 def index():
     return render_template("index.html",
-                           title='Placeholder')
+                           title='Main pages')
+
+@app.route('/manage_schedules')
+def manage_schedules():
+    return render_template("manage_schedules.html", title="Manage")
+
+@app.route('/view_schedules')
+def view_schedules():
+    return render_template("view_schedules.html", title="View")
+
