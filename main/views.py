7d0a31f2489544997dca4f9c50afc12d01ca02f6
george <gbrova@gmail.com>
2015-01-29 15:45:15 -0600

    Built initial UI for schedule list

--- main/views.py
+++ main/views.py
@@ -13,5 +13,40 @@ def manage_schedules():
 
 @app.route('/view_schedules')
 def view_schedules():
-    return render_template("view_schedules.html", title="View")
+    #make a mock schedule list
+    schedule2 = {'status': 'pending',
+                 'name': 'Warren Towers',
+                 'start_date_str': 'February 15, 2015',
+                 'end_date_str': 'March 20, 2015' }
+
+    mock_schedule_list = [
+        {
+            'status': 'pending',
+            'name': 'Warren Towers 1',
+            'start_date_str': 'January 15, 2015',
+            'end_date_str': 'March 20, 2015'
+        },
+        {
+            'status': 'waiting',
+            'name': 'Warren Towers 2',
+            'start_date_str': 'February 15, 2015',
+            'end_date_str': 'March 20, 2015'
+        },
+        {
+            'status': 'active',
+            'name': 'Warren Towers 3',
+            'start_date_str': 'January 10, 2015',
+            'end_date_str': 'March 20, 2015'
+        },
+        {
+            'status': 'expired',
+            'name': 'Warren Towers 4',
+            'start_date_str': 'February 15, 2014',
+            'end_date_str': 'March 20, 2014'
+        },
+    ]
+    
+    return render_template("view_schedules.html", title="View",
+                           schedules=mock_schedule_list)
+
 
