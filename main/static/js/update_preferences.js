9e7395889ee6a0e295647772db55ed58f3e0d634
george <gbrova@gmail.com>
2015-02-24 16:04:43 -0500

    Added placeholder calendar in update_preferences page

---
+++ main/static/js/update_preferences.js
@@ -0,0 +1,5 @@
+window.onload = function()
+{
+    calendarProperties.calendarTableElement = document.getElementById('calendar');
+    renderCalendar(new Date("2014-12-10"), new Date("2015-02-22"));
+}
