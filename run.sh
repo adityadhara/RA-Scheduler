0bc79b4aeca8bc123eba0824ad2934fac3009be5
Aditya Dhara <aditya1992@gmail.com>
2015-05-26 02:10:05 -0400

    Add & at the end of run.sh commands

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-gunicorn --bind unix:/tmp/RAScheduler.sock --access-logfile /var/log/gunicorn/scheduler-access.log --error-logfile /var/log/gunicorn/scheduler-error.log main:app
+gunicorn --bind unix:/tmp/RAScheduler.sock --access-logfile /var/log/gunicorn/scheduler-access.log --error-logfile /var/log/gunicorn/scheduler-error.log main:app &
